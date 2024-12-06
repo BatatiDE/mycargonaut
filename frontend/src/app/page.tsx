79e84426f6977b64133f6e5a6f492b7543f12faf
BatatinGermany <saleh.batati.de@gmail.com>
2024-12-06 10:10:10 +0100

    Initialize frontend with Next.js and Tailwind CSS

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+      <div className="flex items-center justify-center h-screen bg-blue-200">
+        <h1 className="text-4xl font-bold">MyCargonaut Frontend is Ready! 🚀</h1>
+      </div>
+  );
+}
